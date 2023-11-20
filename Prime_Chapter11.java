50283436ff8ac28a80d6a971fc80e69144f3a649
RAKUi7 <124590327+RAKUi7@users.noreply.github.com>
2023-11-20 22:14:48 +0900

    課題11の追加

---
+++ Prime_Chapter11.java
@@ -0,0 +1,31 @@
+package kadai_011;
+
+public class Prime_Chapter11 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		boolean[] newArray = new boolean[101];
+
+		for (int i = 2; i <= 100; i++) {
+			newArray[i] = true;
+		}
+
+		for (int i = 2; i <= 100; i++) {
+			if (newArray[i]) {
+				for (int j = i * 2; j <= 100; j += i) {
+					newArray[j] = false;
+				}
+			}
+		}
+
+		// 結果を表示
+		for (int i = 2; i <= 100; i++) {
+			if (newArray[i]) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
