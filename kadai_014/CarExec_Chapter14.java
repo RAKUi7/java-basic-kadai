0b5eab6e260cff759e7ca5ca29b7e6cc579bc6db
RAKUi7 <124590327+RAKUi7@users.noreply.github.com>
2023-11-26 17:17:33 +0900

    課題14の修正

--- kadai_014/CarExec_Chapter14.java
+++ kadai_014/CarExec_Chapter14.java
@@ -1,13 +1,15 @@
 package kadai_014;
 
 public class CarExec_Chapter14 {
-	
+
 	public static void main(String[] args) {
-		
+
 		Car_Chapter14 cargear = new Car_Chapter14();
-		
-		cargear.gearChange(3);
-		cargear.run();
+
+		for (int i = 2; i <= 5; i++) {
+			cargear.gearChange(i);
+			cargear.run();
+		}
 	}
 
 }
--- kadai_014/Car_Chapter14.java
+++ kadai_014/Car_Chapter14.java
@@ -9,22 +9,36 @@ public class Car_Chapter14 {
 		this.gear = 1;
 		this.speed = 10;
 
-		System.out.println(this.gear);
-		System.out.println(this.speed);
+		System.out.println("ギアは" + this.gear);
+		System.out.println("速度は時速" + this.speed + "kmです。");
 	}
 
 	public void gearChange(int afterGear) {
-		if (1 <= afterGear && afterGear <= 5) {
-			System.out.println("ギア" + this.gear + "からに" + afterGear + "切り替えました。");
-			this.gear = afterGear;
-		} else {
-			System.out.println("ギアの変更はありませんでした。");
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました。");
+		this.gear = afterGear;
+		switch (this.gear) {
+			case 1:
+				this.speed = 10;
+				break;
+			case 2:
+				this.speed = 20;
+				break;
+			case 3:
+				this.speed = 30;
+				break;
+			case 4:
+				this.speed = 40;
+				break;
+			case 5:
+				this.speed = 50;
+				break;
+			default:
+				this.speed = 10;
 		}
 	}
 
 	public void run() {
-		int run = this.gear * 10;
-		System.out.println("速度は時速" + run + "kmです。");
+		System.out.println("速度は時速" + this.speed + "kmです。");
 	}
 
 }
